37037a6da4d2e0b455a83f535a38351e821960cc
Alan Freire <alan.freire.87@gmail.com>
2023-04-24 11:11:50 -0300

    Add:  back button

--- resources/js/Components/Dashboard/Pagination.jsx
+++ resources/js/Components/Dashboard/Pagination.jsx
@@ -1,46 +1,61 @@
 import { Link } from "@inertiajs/react";
 import React from "react";
+import Button from "../Form/Button";
 
-function Pagination({data, count}) {
+function Pagination({data, count, back}) {
     return (
-        <nav className="flex flex-col items-end justify-end gap-2 pt-6">
-            <ul className="flex gap-4 text-gray-500">
-                <li className="">
-                    {data.prev_page_url
-                    ?(<Link href={data.prev_page_url} className="flex items-center justify-between gap-2 p-2 transition border rounded-lg shadow-md hover:bg-gray-50">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-5 h-5" viewBox="0 0 16 16">
-                            <path fillRule="evenodd" d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
-                            <path fillRule="evenodd" d="M12.354 1.646a.5.5 0 0 1 0 .708L6.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
-                        </svg>
-                        Anterior
-                    </Link>)
-                    :(<button type="button" className="flex items-center justify-between gap-2 p-2 text-gray-300 transition border rounded-lg shadow-md cursor-auto">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-5 h-5" viewBox="0 0 16 16">
-                            <path fillRule="evenodd" d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
-                            <path fillRule="evenodd" d="M12.354 1.646a.5.5 0 0 1 0 .708L6.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
-                        </svg>
-                        Anterior
-                    </button>)}
-                </li>
-                <li className="">
-                    {data.next_page_url
-                    ?(<Link href={data.next_page_url} className="flex items-center justify-between gap-2 p-2 transition border rounded-lg shadow-md hover:bg-gray-50">
-                        Próxima
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-5 h-5" viewBox="0 0 16 16">
-                            <path fillRule="evenodd" d="M3.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L9.293 8 3.646 2.354a.5.5 0 0 1 0-.708z"/>
-                            <path fillRule="evenodd" d="M7.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L13.293 8 7.646 2.354a.5.5 0 0 1 0-.708z"/>
-                        </svg>
-                    </Link>)
-                    :(<button type="button" className="flex items-center justify-between gap-2 p-2 text-gray-300 transition border rounded-lg shadow-md cursor-auto">
-                        Próxima
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-5 h-5" viewBox="0 0 16 16">
-                            <path fillRule="evenodd" d="M3.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L9.293 8 3.646 2.354a.5.5 0 0 1 0-.708z"/>
-                            <path fillRule="evenodd" d="M7.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L13.293 8 7.646 2.354a.5.5 0 0 1 0-.708z"/>
-                        </svg>
-                    </button>)}
-                </li>
-            </ul>
-            <p className="flex items-end text-xs font-light text-gray-500">Total de registros<strong className="font-semibold">&nbsp;{count}&nbsp;</strong> - Página<strong className="font-semibold">&nbsp;{data.current_page}&nbsp;</strong>de<strong className="font-semibold">&nbsp;{data.last_page}</strong>.</p>
+        <nav className="flex items-end justify-between gap-2 pt-6">
+            {back
+                ?<Button
+                    href={back}
+                    className={'gap-2'}
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-5 h-5" viewBox="0 0 16 16">
+                        <path fillRule="evenodd" d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5z"/>
+                    </svg>
+                    <span>Voltar</span>
+                </Button>
+                :<span>&nbsp;</span>
+            }
+            <div>
+                <ul className="flex gap-4 text-gray-500">
+                    <li className="">
+                        {data.prev_page_url
+                        ?(<Link href={data.prev_page_url} className="flex items-center justify-between gap-2 p-2 transition border rounded-lg shadow-md hover:bg-gray-50">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-5 h-5" viewBox="0 0 16 16">
+                                <path fillRule="evenodd" d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+                                <path fillRule="evenodd" d="M12.354 1.646a.5.5 0 0 1 0 .708L6.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+                            </svg>
+                            Anterior
+                        </Link>)
+                        :(<button type="button" className="flex items-center justify-between gap-2 p-2 text-gray-300 transition border rounded-lg shadow-md cursor-auto">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-5 h-5" viewBox="0 0 16 16">
+                                <path fillRule="evenodd" d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+                                <path fillRule="evenodd" d="M12.354 1.646a.5.5 0 0 1 0 .708L6.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+                            </svg>
+                            Anterior
+                        </button>)}
+                    </li>
+                    <li className="">
+                        {data.next_page_url
+                        ?(<Link href={data.next_page_url} className="flex items-center justify-between gap-2 p-2 transition border rounded-lg shadow-md hover:bg-gray-50">
+                            Próxima
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-5 h-5" viewBox="0 0 16 16">
+                                <path fillRule="evenodd" d="M3.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L9.293 8 3.646 2.354a.5.5 0 0 1 0-.708z"/>
+                                <path fillRule="evenodd" d="M7.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L13.293 8 7.646 2.354a.5.5 0 0 1 0-.708z"/>
+                            </svg>
+                        </Link>)
+                        :(<button type="button" className="flex items-center justify-between gap-2 p-2 text-gray-300 transition border rounded-lg shadow-md cursor-auto">
+                            Próxima
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-5 h-5" viewBox="0 0 16 16">
+                                <path fillRule="evenodd" d="M3.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L9.293 8 3.646 2.354a.5.5 0 0 1 0-.708z"/>
+                                <path fillRule="evenodd" d="M7.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L13.293 8 7.646 2.354a.5.5 0 0 1 0-.708z"/>
+                            </svg>
+                        </button>)}
+                    </li>
+                </ul>
+                <p className="flex items-end text-xs font-light text-gray-500">Total de registros<strong className="font-semibold">&nbsp;{count}&nbsp;</strong> - Página<strong className="font-semibold">&nbsp;{data.current_page}&nbsp;</strong>de<strong className="font-semibold">&nbsp;{data.last_page}</strong>.</p>
+            </div>
         </nav>
     )
 }
