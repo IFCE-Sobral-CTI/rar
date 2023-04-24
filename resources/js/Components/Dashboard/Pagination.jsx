446ed74fcb88951fedcf81fa5d75a4df43b71a57
Alan Freire <alan.freire.87@gmail.com>
2023-04-24 16:19:38 -0300

    Fix: add support children

--- resources/js/Components/Dashboard/Pagination.jsx
+++ resources/js/Components/Dashboard/Pagination.jsx
@@ -2,21 +2,24 @@ import { Link } from "@inertiajs/react";
 import React from "react";
 import Button from "../Form/Button";
 
-function Pagination({data, count, back}) {
+function Pagination({data, count, back, children}) {
     return (
         <nav className="flex items-end justify-between gap-2 pt-6">
-            {back
-                ?<Button
-                    href={back}
-                    className={'gap-2'}
-                >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-5 h-5" viewBox="0 0 16 16">
-                        <path fillRule="evenodd" d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5z"/>
-                    </svg>
-                    <span>Voltar</span>
-                </Button>
-                :<span>&nbsp;</span>
-            }
+            <div className="flex flex-1 gap-2">
+                {back
+                    ?<Button
+                        href={back}
+                        className={'gap-2'}
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-5 h-5" viewBox="0 0 16 16">
+                            <path fillRule="evenodd" d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5z"/>
+                        </svg>
+                        <span>Voltar</span>
+                    </Button>
+                    :<span>&nbsp;</span>
+                }
+                {children}
+            </div>
             <div>
                 <ul className="flex gap-4 text-gray-500">
                     <li className="">
