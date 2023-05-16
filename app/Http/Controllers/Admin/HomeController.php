a1e7217c40a4f8e06f3d7b0c637d20cdba6c3fad
Alan Freire <alan.freire.87@gmail.com>
2023-05-16 16:04:56 -0300

    Fix: data for charts

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -3,6 +3,12 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Course;
+use App\Models\Dispatch;
+use App\Models\PrintQueue;
+use App\Models\Report;
+use App\Models\Requirement;
+use App\Models\Semester;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -15,6 +21,36 @@ class HomeController extends Controller
      */
     public function index(Request $request): Response
     {
-        return Inertia::render('Dashboard');
+        return Inertia::render('Dashboard', [
+            'can' => [
+                'print_queue_view' => $request->user()->can('print_queues.show')
+            ],
+            'requirements' => [
+                'renovations' => Requirement::getRenovationsCount(),
+                'renovations_chart' => Requirement::getDataOfRenovationsForChart(),
+                'reprint' => Requirement::getReprintCount(),
+                'reprint_chart' => Requirement::getDataOfReprintForChart(),
+                'first_copy' => Requirement::getFirstCopyCount(),
+                'first_copy_chart' => Requirement::getDataOfFirstCopyForChart(),
+                'count' => Requirement::getCount(),
+            ],
+            'dispatches' => [
+                'count' => Dispatch::getCount(),
+                'chart' => Dispatch::getDataForChart(),
+            ],
+            'print_queue' => [
+                'list' => PrintQueue::getDataForDashboardChart(),
+                'count' => PrintQueue::count()
+            ],
+            'print' => [
+                'count' => Report::getRequirementCount(),
+                'printed' => Report::getPrinted(),
+                'chart' => Report::getDataForChart(),
+            ],
+            'semester' => Semester::getCurrent(),
+            'courses' => [
+                'chart' => Course::getDataForChart()
+            ]
+        ]);
     }
 }
